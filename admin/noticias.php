26ac52470e63973daee3b9dde443bde329958e1b
Emerson Full-Stack Senac <emerson.agomes1@senacsp.edu.br>
2025-11-18 17:19:14 -0300

    incluir info em pagina de noticias com contador

--- admin/noticias.php
+++ admin/noticias.php
@@ -11,7 +11,7 @@ $noticiaServico = new NoticiaServico();
 
 try {
 	$noticias = $noticiaServico->buscar();
-	Utils::dump($noticias);
+	
 } catch (\Throwable $e) {
 	$erro = "Erro ao buscar usuários. <br>".$e->getMessage();
 }
@@ -25,7 +25,7 @@ require_once "../includes/cabecalho-admin.php";
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
 		
-		<h2 class="text-center">Notícias <span class="badge bg-dark">X</span></h2>
+		<h2 class="text-center">Notícias <span class="badge bg-dark"><?=count($noticias)?></span></h2>
 
 		<?php if($erro): ?>
 		<p class="alert alert-danger text-center" > <?=$erro?> </p>
@@ -52,27 +52,29 @@ require_once "../includes/cabecalho-admin.php";
 
 				<tbody>
 
-
+				<?php foreach($noticias as $noticia){ ?>
 					<tr>
-                        <td> Título... </td>
-                        <td> Data... </td>
-						<td> Autor... </td>
+                        <td><?=$noticia['titulo']?></td>
+                        <td><?=$noticia['data']?></td>
+                        <td><?=$noticia['autor']?></td>
+                        
+						
 						
 
 						<td class="text-center">
 							<a class="btn btn-warning" 
-							href="noticia-atualiza.php">
+							href="noticia-atualiza.php?id=<?=$noticia['id']?>">
 							<i class="bi bi-pencil"></i> Atualizar
 							</a>
 						</td>
 						<td>
 							<a class="btn btn-danger excluir" 
-							href="noticia-exclui.php">
+							href="noticia-exclui.php?id=<?=$noticia['id']?>">
 							<i class="bi bi-trash"></i> Excluir
 							</a>
 						</td>
 					</tr>
-
+				<?php } ?>	
 				</tbody>                
 			</table>
 	</div>
