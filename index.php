fab23a298ea0d584d51797dbe109b2ee24253c41
Emerson Full-Stack Senac <emerson.agomes1@senacsp.edu.br>
2025-11-25 17:18:13 -0300

    tamanho de imagens

--- index.php
+++ index.php
@@ -22,18 +22,21 @@ require_once "includes/cabecalho.php";
 
     <!-- INÍCIO Card -->
 
-    <div class="col-md-6 my-1 px-md-1">
-        <article class="card shadow-sm h-100">
-            <a href="noticia.php" class="card-link">
-                <img src="images/abstrato.jpg" class="card-img-top" alt="Imagem de capa do card">
-                <div class="card-body">
-                    <h3 class="fs-4 card-title">Título da notícia...</h3>
-                    <p class="card-text">Resumo da notícia...</p>
-                </div>
-            </a>
-        </article>
-    </div>
+    <?php foreach($noticias as $noticia){ ?>
 
+        <div class="col-md-6 my-1 px-md-1">
+            <article class="card shadow-sm h-100">
+                <a href="noticia.php" class="card-link">
+                    <img src="images/<?= $noticia['imagem'] ?>" height="500px" class="card-img-top" alt="Imagem de capa do card">
+                    <div class="card-body">
+                        <h3 class="fs-4 card-title"><?= $noticia['titulo'] ?></h3>
+                        <p class="card-text"><?= $noticia['resumo'] ?></p>
+                    </div>
+                </a>
+            </article>
+        </div>
+
+    <?php } ?>
     <!-- FIM Card -->
 
 </div>
