f54c33d3d31c4eff0e7be3abf5f3d8d902fa89fb
Emerson Full-Stack Senac <emerson.agomes1@senacsp.edu.br>
2025-11-25 14:25:41 -0300

    função excluir

--- src/Services/NoticiaServico.php
+++ src/Services/NoticiaServico.php
@@ -125,4 +125,24 @@ class NoticiaServico {
 
     }
 
+    //admin/noticia-exclui.php
+    public function excluir(int $idNoticia, int $idUsuario, string $tipoUsuario):void {
+
+        if ($tipoUsuario === 'admin') {
+            $sql = "DELETE FROM usuarios WHERE id = :id";
+        } else {
+            $sql = "DELETE FROM usuarios WHERE id = :id AND usuario_id = :usuario_id";
+        }
+
+        $consulta = $this->conexao->prepare($sql);
+        $consulta->bindValue(":id", $idNoticia->getId());
+
+        if ($tipoUsuario !== 'admin') {
+            $consulta->bindValue(":usuario_id", $idUsuario->getUsuarioId());
+        }
+
+        $consulta->execute();
+
+    }    
+
 }
